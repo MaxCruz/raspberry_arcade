ead934c03148c80e460638eccee5014443c946b2
Max Cruz <max.raul@gmail.com>
2016-02-22 02:29:13 -0500

    Test with default BTN_JOYSTICK and AXIS

--- gpio_joystick_driver.py
+++ gpio_joystick_driver.py
@@ -7,34 +7,34 @@ import RPi.GPIO as GPIO
 
 # This dictionary contains the map for the operative system events, the inputs in
 # the GPIO interface for the Raspberry PI 2 and the function state in the arcade machine
-#   [FUNCTION] = [PIN, GPIO, STATE, EVENT]
+#   [FUNCTION] = [PIN, GPIO, STATE, EVENT, IS_AXIS]
 # When a input signal in the GPIO interface is set to HIGH, the corresponding event is send
 # to the operative system and the state is set to true in runtime until the signal is in LOW
 interface = {
-    "P1_UP": [3, 2, False, uinput.KEY_UP],
-    "P1_DOWN": [5, 3, False, uinput.KEY_DOWN],
-    "P1_LEFT": [7, 4, False, uinput.KEY_LEFT],
-    "P1_RIGHT": [11, 17, False, uinput.KEY_RIGHT],
-    "P1_A": [13, 27, False, uinput.KEY_0],
-    "P1_B": [15, 22, False, uinput.KEY_2],
-    "P1_C": [21, 9, False, uinput.KEY_3],
-    "P1_D": [23, 11, False, uinput.KEY_4],
-    "P1_X": [29, 5, False, uinput.KEY_5],
-    "P1_Y": [31, 6, False, uinput.KEY_6],
-    "P1_SELECT": [33, 13, False, uinput.KEY_7],
-    "P1_START": [35, 19, False, uinput.KEY_8],
-    "P2_UP": [37, 26, False, uinput.KEY_HOME],
-    "P2_DOWN": [8, 14, False, uinput.KEY_END],
-    "P2_LEFT": [10, 15, False, uinput.KEY_DELETE],
-    "P2_RIGHT": [12, 18, False, uinput.KEY_PAGEDOWN],
-    "P2_A": [18, 24, False, uinput.KEY_A],
-    "P2_B": [22, 25, False, uinput.KEY_Z],
-    "P2_C": [24, 8, False, uinput.KEY_S],
-    "P2_D": [26, 7, False, uinput.KEY_X],
-    "P2_X": [32, 12, False, uinput.KEY_D],
-    "P2_Y": [36, 16, False, uinput.KEY_C],
-    "P2_SELECT": [38, 20, False, uinput.KEY_F],
-    "P2_START": [40, 21, False, uinput.KEY_V]
+    "P1_UP": [3, 2, False, uinput.ABS_Y, True],
+    "P1_DOWN": [5, 3, False, uinput.ABS_Y, True],
+    "P1_LEFT": [7, 4, False, uinput.ABS_X, True],
+    "P1_RIGHT": [11, 17, False, uinput.ABS_X, True],
+    "P1_A": [13, 27, False, uinput.BTN_1, False],
+    "P1_B": [15, 22, False, uinput.BTN_2, False],
+    "P1_C": [21, 9, False, uinput.BTN_3, False],
+    "P1_D": [23, 11, False, uinput.BTN_4, False],
+    "P1_X": [29, 5, False, uinput.BTN_5, False],
+    "P1_Y": [31, 6, False, uinput.BTN_6, False],
+    "P1_SELECT": [33, 13, False, uinput.BTN_0, False],
+    "P1_START": [35, 19, False, uinput.BTN_JOYSTICK, False],
+    "P2_UP": [37, 26, False, uinput.KEY_UP, False],
+    "P2_DOWN": [8, 14, False, uinput.KEY_DOWN, False],
+    "P2_LEFT": [10, 15, False, uinput.KEY_LEFT, False],
+    "P2_RIGHT": [12, 18, False, uinput.KEY_RIGHT, False],
+    "P2_A": [18, 24, False, uinput.KEY_1, False],
+    "P2_B": [22, 25, False, uinput.KEY_2, False],
+    "P2_C": [24, 8, False, uinput.KEY_3, False],
+    "P2_D": [26, 7, False, uinput.KEY_4, False],
+    "P2_X": [32, 12, False, uinput.KEY_5, False],
+    "P2_Y": [36, 16, False, uinput.KEY_6, False],
+    "P2_SELECT": [38, 20, False, uinput.KEY_7, False],
+    "P2_START": [40, 21, False, uinput.KEY_8, False]
 }
 
 
@@ -51,17 +51,22 @@ def input_setup(item_list):
 
 # Read the value configured in the interface and emit the corresponding event
 def input_read(key, item_list):
-    if GPIO.input(item_list[0]):
-        device.emit_click(item_list[3])
+    if (not item_list[2]) and (not GPIO.input(item_list[0])):
+        item_list[2] = True
+        if item_list[4] and key.endswith("UP"):
+            device.emit(item_list[3], 0)
+        if item_list[4] and key.endswith("DOWN"):
+            device.emit(item_list[3], 255)
+        else:
+            device.emit(item_list[3], 1)
         print "KEY {} PRESS".format(key)
-    # if (not item_list[2]) and (not GPIO.input(item_list[0])):
-    #    item_list[2] = True
-    #    device.emit(item_list[3], 1)
-    #    print "KEY {} PRESS".format(key
-    # if item_list[2] and GPIO.input(item_list[0]):
-    #    item_list[2] = False
-    #    device.emit(item_list[3], 0)
-    #    print "KEY {} PRESS".format(key)
+    if item_list[2] and GPIO.input(item_list[0]):
+        item_list[2] = False
+        if item_list[4]:
+            device.emit(item_list[3], 128)
+        else:
+            device.emit(item_list[3], 0)
+        print "KEY {} RELEASE".format(key)
     return
 
 
@@ -72,15 +77,43 @@ GPIO.setmode(GPIO.BOARD)
 map(input_setup, interface.values())
 
 # Define input events to associate with the device
-events = map(get_events, interface.values())
+# events = set(map(get_events, interface.values()))
+events = (
+    uinput.ABS_X + (0, 255, 0, 0),
+    uinput.ABS_Y + (0, 255, 0, 0),
+    uinput.BTN_JOYSTICK,
+    uinput.BTN_0,
+    uinput.BTN_1,
+    uinput.BTN_2,
+    uinput.BTN_3,
+    uinput.BTN_4,
+    uinput.BTN_5,
+    uinput.BTN_6,
+    uinput.KEY_UP,
+    uinput.KEY_DOWN,
+    uinput.KEY_LEFT,
+    uinput.KEY_RIGHT,
+    uinput.KEY_0,
+    uinput.KEY_1,
+    uinput.KEY_2,
+    uinput.KEY_3,
+    uinput.KEY_4,
+    uinput.KEY_5,
+    uinput.KEY_6,
+    uinput.KEY_7,
+    uinput.KEY_8
+)
 device = uinput.Device(events)
+
+device.emit(uinput.ABS_X, 128, syn=False)
+device.emit(uinput.ABS_Y, 128)
 
 # Start main infinite thread for read the inputs and emit the events
 try:
     while True:
         for k, v in interface.iteritems():
             input_read(k, v)
-        time.sleep(0.2)
+        time.sleep(0.04)
 except KeyboardInterrupt:
     print "BYE"
 finally:
